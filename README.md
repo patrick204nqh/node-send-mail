2cceea9823f5f64826cef1a10b2b8c2bbfbec7dd
patrick <patrick204nqh@gmail.com>
2020-07-31 22:21:48 +0700

    config nodemailer

---
+++ README.md
@@ -0,0 +1,4 @@
+# Patrick's Project (Node.js)
+
+1. Nodemailer using gmail account
+   > https://www.youtube.com/watch?v=Va9UKGs1bwI&frags=pl%2Cwn
--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const nodemailer = require('nodemailer');
 
+require('dotenv').config();
+
 const app = express();
 
 // View engine setup
@@ -27,4 +29,43 @@ app.get('/', (req, res) => {
   res.render('contact');
 });
 
-app.listen(3000, () => console.log('Server started...'))
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+  `;
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL,
+    to: 'patrick204nqh@gmail.com',
+    subject: 'Testing',
+    text: 'hello, world',
+    html: output
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log('Error Occurs', err)
+    } else {
+      console.log('Email Sent');
+      res.render('contact');
+    }
+  })
+});
+
+app.listen(3000, () => console.log('Server started...'));
